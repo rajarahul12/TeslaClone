1feaaaaed66e44b0926f54f9737773175f0ee14f
rajarahul12 <maddula.rajarahul@gmail.com>
2020-12-25 21:43:52 +0530

    Add CarItem component

---
+++ Components/CarItem/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, ImageBackground } from "react-native";
+import styles from "./styles";
+
+export default function CarItem(props) {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={require("../../assets/images/ModelS.jpeg")}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subTitle}>Starting at $69,420</Text>
+      </View>
+    </View>
+  );
+}
